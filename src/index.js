55d577e59bad9e9bc654a7a0698a5955911bbbc2
Rinat Kamaletdinov <sky2208@ya.ru>
2021-03-25 21:09:26 +0400

    FIX

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 export default class Team {
-    constructor() {
-        this.members = new Set();
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(obj) {
+    function deepEqual(obj1, obj2) {
+      return JSON.stringify(obj1) === JSON.stringify(obj2);
+    }
+    /* eslint-disable-next-line */
+    for (const i of this.members) {
+      if (deepEqual(i, obj)) { throw new Error('Данный герой уже добавлен'); }
     }
 
-    add(obj) {
-        for(const i of this.members) {
-            if(i.name == obj.name) 
-               throw new Error('Данный герой уже добавлен');
-        } 
+    this.members.add(obj);
+  }
 
-        this.members.add(obj);
+  addAll(...obj) {
+    const unique = obj.filter(((set) => (f) => !set.has(f.name) && set.add(f.name))(new Set()));
+    /* eslint-disable-next-line */
+    for (const i of unique) {
+      this.members.add(i);
     }
+  }
 
-    addAll(...obj) {
-        let unique = obj.filter((set => f => !set.has(f.name) && set.add(f.name))(new Set));
-        for(let i of unique) {
-            this.members.add(i);
-        }
-    }
-
-    toArray() {
-        const arr = Array.from(this.members);
-        return arr;
-    }
+  toArray() {
+    const arr = Array.from(this.members);
+    return arr;
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,33 +1,53 @@
-import Team from "./index";
+/* eslint-disable */
+import Team from './index';
 
-test("test", ()=> {
-    const team = new Team();
-    team.add({name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25});
-    expect(team.members.size).toEqual(1)
+test('test', () => {
+  const team = new Team();
+  team.add({
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  });
+  expect(team.members.size).toEqual(1);
 });
 
+test('test ', () => {
+  const team = new Team();
+  team.add({
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  });
+  expect(() => team.add({
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  })).toThrow(Error);
+});
 
+test('test', () => {
+  const team = new Team();
+  team.addAll({
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  },
+  {
+    name: 'igor', type: 'Zombie', health: 100, level: 1, attack: 25, defence: 25,
+  },
+  {
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  });
+  expect(team.members.size).toEqual(2);
+});
 
-    test('test ', () => {
-        const team = new Team();
-        team.add({name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25})
-        expect(() => team.add({name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25})).toThrow(Error);
-     });
-
-
-     test("test", ()=> {
-        const team = new Team();
-        team.addAll({name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25}, 
-        {name: "igor", type: "Zombie", health:100, level:1, attack:25, defence:25}, 
-        {name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25});
-        expect(team.members.size).toEqual(2)
-    });
-
-    test("test", ()=> {
-        const team = new Team();
-        team.addAll({name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25}, 
-        {name: "igor", type: "Zombie", health:100, level:1, attack:25, defence:25}, 
-        {name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25});
-        expect(team.toArray()).toEqual([{name: "ivan", type: "Bowman", health:100, level:1, attack:25, defence:25}, 
-        {name: "igor", type: "Zombie", health:100, level:1, attack:25, defence:25}])
-    });
+test('test', () => {
+  const team = new Team();
+  team.addAll({
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  },
+  {
+    name: 'igor', type: 'Zombie', health: 100, level: 1, attack: 25, defence: 25,
+  },
+  {
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  });
+  expect(team.toArray()).toEqual([{
+    name: 'ivan', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  },
+  {
+    name: 'igor', type: 'Zombie', health: 100, level: 1, attack: 25, defence: 25,
+  }]);
+});
